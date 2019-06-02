cfc1ad50e197045119e586e8a873556546d4e4ee
marcus.trenton <elpollodiablos@hotmail.com>
2019-06-01 23:35:15 -0700

    Initial commit for an empty project. Derived data should be in the .gitignore file.

---
+++ RockPaperScissors/Source/RockPaperScissors/RockPaperScissorsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RockPaperScissorsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROCKPAPERSCISSORS_API ARockPaperScissorsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
