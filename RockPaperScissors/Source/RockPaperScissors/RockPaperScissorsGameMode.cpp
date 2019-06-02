9181d9fb6dfaf53d7bb53ab1a90033c9ebbbb219
marcus.trenton <elpollodiablos@hotmail.com>
2019-06-02 15:29:52 -0700

    Switched over to a Puzzle Cpp Template

---
+++ RockPaperScissors/Source/RockPaperScissors/RockPaperScissorsGameMode.cpp
@@ -0,0 +1,13 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "RockPaperScissorsGameMode.h"
+#include "RockPaperScissorsPlayerController.h"
+#include "RockPaperScissorsPawn.h"
+
+ARockPaperScissorsGameMode::ARockPaperScissorsGameMode()
+{
+	// no pawn by default
+	DefaultPawnClass = ARockPaperScissorsPawn::StaticClass();
+	// use our own player controller class
+	PlayerControllerClass = ARockPaperScissorsPlayerController::StaticClass();
+}
